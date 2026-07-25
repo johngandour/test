a5cc6bb927249a7455ade61478bc92b13e1ae430
test11 <166078457+johngandour@users.noreply.github.com>
2026-07-25 16:22:26 +0300

    remove probe

--- diffprobe.js
+++
@@ -1,5 +0,0 @@
-DIFFCAN1 <img src=x onerror=alert(1)>
-DIFFCAN2 </code></div><img src=x onerror=alert(2)>
-DIFFCAN3 <script>alert(3)</script>
-DIFFCAN4 "><svg/onload=alert(4)>
-DIFFCAN5 </span></td></tr><img src=x onerror=alert(5)>
